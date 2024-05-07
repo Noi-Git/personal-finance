4c4f8c0962aeffd1ca6f694af08d4091e7c981db
Noi-Git <noi.patsin@gmail.com>
2024-05-07 01:41:31 -0400

    create a logout button when clicked change to route to logout

--- pf-frontend/src/components/Nav.jsx
+++ pf-frontend/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom'
+import { Form, NavLink } from 'react-router-dom'
 import logomark from '../assets/logomark.svg'
 const Nav = ({ userName }) => {
   return (
@@ -9,6 +9,14 @@ const Nav = ({ userName }) => {
           <img src={logomark} alt='' height={30} />
           <span>Personal Finance</span>
         </NavLink>
+        {/* if there is user -- show form */}
+        {userName && (
+          <Form method='post' action='/logout'>
+            <button type='submit' className='btn btn--warning'>
+              <span>Delete User</span>
+            </button>
+          </Form>
+        )}
       </nav>
     </>
   )
