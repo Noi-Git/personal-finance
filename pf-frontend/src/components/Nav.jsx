2e0ab6088e1668cb50485868c6254d0243c07fc4
Noi-Git <noi.patsin@gmail.com>
2024-05-07 01:27:32 -0400

    create basic navbar and pass in props form Main component to Nav component

---
+++ pf-frontend/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = ({ userName }) => {
+  return (
+    <>
+      <div className='nav'>{userName}</div>
+    </>
+  )
+}
+
+export default Nav
--- pf-frontend/src/layouts/Main.jsx
+++ pf-frontend/src/layouts/Main.jsx
@@ -1,5 +1,7 @@
 import { Outlet, useLoaderData } from 'react-router-dom'
 import { fetchData } from '../helpers'
+import wave from '../assets/wave.svg'
+import Nav from '../components/Nav'
 
 //loader
 // eslint-disable-next-line react-refresh/only-export-components
@@ -10,10 +12,16 @@ export function mainLoader() {
 
 const Main = () => {
   const { userName } = useLoaderData()
+
   return (
     <>
-      <div>{userName}</div>
-      <Outlet />
+      <div className='layout'>
+        <Nav userName={userName} />
+        <main>
+          <Outlet />
+        </main>
+        <img src={wave} alt='' />
+      </div>
     </>
   )
 }
