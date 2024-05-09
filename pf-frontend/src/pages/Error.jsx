d72bd67141c8156ec1244a28e09e17941aa6c243
Noi-Git <noi.patsin@gmail.com>
2024-05-09 15:12:57 -0400

    create custom error page

--- pf-frontend/src/pages/Error.jsx
+++ pf-frontend/src/pages/Error.jsx
@@ -1,5 +1,28 @@
+import { Link, useRouteError } from 'react-router-dom'
+import { HomeIcon, ArrowUturnLeftIcon } from '@heroicons/react/24/solid'
+
 const Error = () => {
-  return <div>Error</div>
+  const error = useRouteError()
+  console.log(error)
+
+  return (
+    <>
+      <div className='error'>
+        <h1>Oh oh! We&lsquo;ve got a problem</h1>
+        <p>{error.message || error.status || error.statusText}</p>
+        <div className='flex-md'>
+          <button>
+            <ArrowUturnLeftIcon width={20} />
+            <span>Go Back</span>
+          </button>
+          <Link to='/' className='btn btn--dark'>
+            <HomeIcon width={20} />
+            <span>Go home</span>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Error
