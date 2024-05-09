fbbf51ac51743c56258d6a659bf3396f60296941
Noi-Git <noi.patsin@gmail.com>
2024-05-09 19:07:39 -0400

    use the fetcher to track state and uses useRef to clear and focus form

--- pf-frontend/src/components/AddBudgetForm.jsx
+++ pf-frontend/src/components/AddBudgetForm.jsx
@@ -1,12 +1,26 @@
 import { CurrencyDollarIcon } from '@heroicons/react/24/solid'
-import { Form } from 'react-router-dom'
+import { useEffect, useRef } from 'react'
+import { Form, useFetcher } from 'react-router-dom'
 
 const AddBudgetForm = () => {
+  const fetcher = useFetcher()
+  const isSubmitting = fetcher.state === 'submitting'
+
+  const formRef = useRef()
+  const focusRef = useRef()
+
+  useEffect(() => {
+    if (isSubmitting) {
+      formRef.current.reset()
+      focusRef.current.focus()
+    }
+  }, [isSubmitting])
+
   return (
     <>
       <div className='form-wrapper'>
         <h2 className='h3'>Create budget</h2>
-        <Form method='post' className='grid-sm'>
+        <fetcher.Form method='post' className='grid-sm' ref={formRef}>
           <div className='grid-xs'>
             <label htmlFor='newBudget'>Budget Name</label>
             <input
@@ -14,6 +28,7 @@ const AddBudgetForm = () => {
               name='newBudget'
               id='newBudget'
               placeholder='e.g., Groceries'
+              ref={focusRef}
               required
             />
           </div>
@@ -34,7 +49,7 @@ const AddBudgetForm = () => {
             <span>Create budget</span>
             <CurrencyDollarIcon width={20} />
           </button>
-        </Form>
+        </fetcher.Form>
       </div>
     </>
   )
