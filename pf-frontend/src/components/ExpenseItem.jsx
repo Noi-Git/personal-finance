82948591553207470bbed7ad145d120ef07cbf2b
Noi-Git <noi.patsin@gmail.com>
2024-05-10 17:13:08 -0400

    useFetcher Form and add button

--- pf-frontend/src/components/ExpenseItem.jsx
+++ pf-frontend/src/components/ExpenseItem.jsx
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom'
+import { Link, useFetcher } from 'react-router-dom'
 import {
   formatCurrency,
   formatDateToLocalString,
   getAllMatchingItems,
 } from '../helpers'
-
+import { TrashIcon } from '@heroicons/react/24/solid'
 // eslint-disable-next-line react/prop-types
 const ExpenseItem = ({ expense }) => {
+  const fetcher = useFetcher()
   const budget = getAllMatchingItems({
     category: 'budgets',
     key: 'id',
+    // eslint-disable-next-line react/prop-types
     value: expense.budgetId,
   })[0]
   console.log('budget--', budget)
@@ -24,7 +26,28 @@ const ExpenseItem = ({ expense }) => {
       <td>{formatDateToLocalString(expense.createdAt)}</td>
       {/* eslint-disable-next-line react/prop-types */}
       <td>
-        <Link to={`/budget/${budget.id}`}>{budget.name}</Link>
+        <Link
+          to={`/budget/${budget.id}`}
+          style={{
+            '--accent': budget.color,
+          }}
+        >
+          {budget.name}
+        </Link>
+      </td>
+      <td>
+        <fetcher.Form method='post'>
+          <input type='hidden' name='_action' value='deleteExpense' />
+          {/* eslint-disable-next-line react/prop-types */}
+          <input type='hidden' name='expenseId' value={expense.id} />
+          <button
+            className='btn btn--warning'
+            // eslint-disable-next-line react/prop-types
+            aria-label={`Delete ${expense.name} expense`}
+          >
+            <TrashIcon width={20} />
+          </button>
+        </fetcher.Form>
       </td>
     </>
   )
