c2f3fdf593c8b643e8ebc409ecc0bbc2ce500aa9
Noi-Git <noi.patsin@gmail.com>
2024-05-10 19:16:28 -0400

    bring in budget details to show in the individual page

--- pf-frontend/src/components/ExpenseItem.jsx
+++ pf-frontend/src/components/ExpenseItem.jsx
@@ -19,7 +19,7 @@ const ExpenseItem = ({ expense }) => {
   return (
     <>
       {/* eslint-disable-next-line react/prop-types */}
-      <td className='align-left'>{formatCurrency(expense.name)}</td>
+      <td>{formatCurrency(expense.name)}</td>
       {/* eslint-disable-next-line react/prop-types */}
       <td>{formatCurrency(expense.amount)}</td>
       {/* eslint-disable-next-line react/prop-types */}
--- pf-frontend/src/pages/BudgetPage.jsx
+++ pf-frontend/src/pages/BudgetPage.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from 'react-router-dom'
 import { getAllMatchingItems } from '../helpers'
 import BudgetItem from '../components/BudgetItem'
 import AddExpenseForm from '../components/AddExpenseForm'
+import { Table } from '../components/Table'
 
 //Loader
 export async function budgetLoader({ params }) {
@@ -10,15 +11,22 @@ export async function budgetLoader({ params }) {
     key: 'id',
     value: params.id,
   })[0]
+
+  const expenses = await getAllMatchingItems({
+    category: 'expenses',
+    key: 'budgetId',
+    value: params.id,
+  })
+
   if (!budget) {
     throw new Error(`The budget you's trying to find doesn't exist`)
   }
 
-  return { budget }
+  return { budget, expenses }
 }
 
 const BudgetPage = () => {
-  const { budget } = useLoaderData()
+  const { budget, expenses } = useLoaderData()
 
   return (
     <>
@@ -30,6 +38,14 @@ const BudgetPage = () => {
           <BudgetItem budget={budget} />
           <AddExpenseForm budgets={[budget]} />
         </div>
+        {expenses && expenses.length > 0 && (
+          <div className='grid-md'>
+            <h2>
+              <span className='accent'>{budget.name}</span> Espenses
+            </h2>
+            <Table expenses={expenses} />
+          </div>
+        )}
       </div>
     </>
   )
